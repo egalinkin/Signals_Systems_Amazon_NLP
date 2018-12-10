6373ae114524af5d8266149f168bbdc8528149a1
Erick Galinkin <erick.galinkin@gmail.com>
2018-12-10 05:40:48 -0500

    Version in progress - model is still running for approximately 13 more hours. Check to see if README.md update takes embedded graphics.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Signals_Systems_Amazon_NLP
 Natural Language Processing project for 625.260 - Fall 2018
+
+## What is this?
+This is my final project for Introduction to Signals and Systems, Fall 2018. Using the Keras library, I've built a Recurrent Neural Network which leverages Long Short-Term Memory to perform an analysis on the Amazon Review dataset organized and deduplicated by Julian McAuley
+
+This neural network seeks to take up to 300 words of a review and output an estimated score based on those reviews. Although we are not solving a novel problem, it provides a good framework for sentiment classification that may boil down a longwinded review (or README.md) into an easy to understand 1-5 (or in the case of our model, 0-4) score. We could likely extend this architecture into a recommender system off of the same dataset.
+
+## Network architecture
+The network architecture here is fairly simple, using the 300d glove vectors provided by [GloVe](https://nlp.stanford.edu/projects/glove/) at Stanford university to form an embedding for the [Amazon product data](http://jmcauley.ucsd.edu/data/amazon/) graciously curated and provided by Julian McAuley.
+This is then passed to two Keras Long Short-Term Memory layers, followed by Dropout layer, which will "deactivate" some number of neurons (in this case, 20% of our neurons) and a third LSTM layer before being evaluated by a softmax to assess what "score" the input should receive. 
+
+Long Short-Term Memory is particularly interesting from the perspective of feedback systems. Essentially, they are a standard neuron within a neural net, but also contain "gates" which allow the neural net to maintain some level of context around sequence data. 
+![](/Users/egalinkin/Downloads/Diagram-of-LSTM-block.png)
+
+<small>Credit to Jitong Chen and DeLiang Wang for the diagram.</small>
+
+The input gate i<sub>t</sub> takes our old cell state, C<sub>t</sub> and gives us a new candidate value. By "remembering" the state of the previous cell or cells, 
+### Citations
+R. He, J. McAuley. Modeling the visual evolution of fashion trends with one-class collaborative filtering. WWW, 2016
+
+J. McAuley, C. Targett, J. Shi, A. van den Hengel. Image-based recommendations on styles and substitutes. SIGIR, 2015
+
+J. Chen, D. Wang. Long Short-Term Memory for Speaker Generalization in Supervised Speech Separation. 2016
---
+++ model.py
@@ -0,0 +1,114 @@
+import sys
+import numpy as np
+import json
+from keras.preprocessing.text import Tokenizer
+from keras.layers import Embedding, Flatten, Dense, LSTM, Dropout, Activation
+from keras.models import Sequential
+from keras.utils import to_categorical
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
+
+
+# With credit to keras.io team for the tokenization parts of this code.
+def load_data(datapath, glovepath, sample_size=25000):
+    maxlen = 300  # Only use the first 300 words of each review
+    vocab_size = 10000  # Have a 10000 word vocabulary
+
+    print("Loading data...")
+    labels = list()
+    texts = list()
+    with open(datapath, "r") as f:
+        if sample_size > 0:  # For development, we use a reduced sample of 25000 to make sure the code works.
+            for i in range(sample_size):
+                line = json.loads(f.readline())
+                labels.append(int(line["overall"]) - 1)  # Want to convert values to int, since we're categorizing.
+                texts.append(line["reviewText"])
+        else:
+            for line in f:
+                line = json.loads(line)
+                labels.append(int(line["overall"]) - 1)  # Want to convert values to int, since we're categorizing.
+                texts.append(line["reviewText"])
+
+    embeddings_index = dict()
+    with open(glovepath, "r") as f:
+        for line in f.readlines():
+            values = line.split()
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = coefs
+    print("Data loaded!")
+
+    tokenizer = Tokenizer(num_words=vocab_size)
+    tokenizer.fit_on_texts(texts)
+    sequences = tokenizer.texts_to_sequences(texts)
+
+    data = pad_sequences(sequences, maxlen)
+    labels = np.asarray(labels)
+
+    indices = np.arange(data.shape[0])  # Shuffling dataset using the number of samples
+    np.random.shuffle(indices)
+    data = data[indices]
+    labels = labels[indices]  # Mapping is preserved by using the same indices.
+
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=.3)  # Use 30% of data for testing.
+
+    # Prepare embedding matrix
+    word_index = tokenizer.word_index
+    embedding_matrix = np.zeros((vocab_size, 300))  # Our embedding vectors are 300-dimensional.
+    for word, i in word_index.items():
+        if i < vocab_size:
+            embedding_vector = embeddings_index.get(word)
+            if embedding_vector is not None:
+                embedding_matrix[i] = embedding_vector
+    return x_train, x_test, y_train, y_test, embedding_matrix
+
+
+def build_model(embeddings):
+    model = Sequential()
+    # 10000 word vocabulary, 300-dimensional embedding, 200 words.
+    model.add(Embedding(10000, 300, weights=[embeddings], input_length=300))
+    # Long Short Term Memory layer - explained in README.md
+    model.add(LSTM(300, return_sequences=True))
+    model.add(LSTM(300, return_sequences=True))
+    model.add(Dropout(0.2))  # Dropout can help improve training on a model and reduce overfitting
+    model.add(LSTM(300, return_sequences=False))
+    model.add(Dense(5, input_dim=300, activation='softmax'))  # Output our predicted score.
+    print(model.summary())
+    return model
+
+
+def plot_history(history):
+    accuracy = history.history['acc']
+    validation_accuracy = history.history['val_acc']
+    loss = history.history['loss']
+    validation_loss = history.history['val_loss']
+    epochs = range(1, len(accuracy) + 1)
+    plt.plot(epochs, accuracy, 'bo', label="Training accuracy")
+    plt.plot(epochs, validation_accuracy, 'b', label="Validation accuracy")
+    plt.title("Training and validation accuracy")
+    plt.legend()
+    plt.figure()
+    plt.plot(epochs, loss, 'bo', label="Training loss")
+    plt.plot(epochs, validation_loss, 'b', label="Validation loss")
+    plt.title("Training and validation loss")
+    plt.legend()
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    sample_size = 25000
+    if len(sys.argv) == 2:
+        sample_size = int(sys.argv[1])
+    X_train, X_test, y_train, y_test, embeddings = load_data("aggressive_dedup.json", "glove_300d.txt", sample_size)
+    print()
+    print("Building model...")
+    model = build_model(embeddings)
+    print("Running!")
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer=Adam(lr=0.001),
+                  metrics=['accuracy'])
+    history = model.fit(X_train, y_train, batch_size=128, epochs=25, validation_data=(X_test, y_test))
+    plot_history(history)
